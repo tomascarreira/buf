8da809d7a19c043c258bb0a0cc2b58ad3f44d5f6
tomascarreira <carreira_1012@hotmail.com>
2023-01-20 13:58:34 +0000

    Remove unnecessary path::{Path, PathBuf} stuff

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-use std::{env, fs::{File, OpenOptions}, io, path::{Path, PathBuf}};
+use std::{env, fs::{File, OpenOptions}, io};
 
 fn main() {
     let mut args = env::args(); 
     args.next();
     
-    let file_arg = args.next().expect("The user has to supply a path to a file");
-    let file_path = Path::new(&file_arg);
+    let file_path = args.next().expect("The user has to supply a path to a file");
 
-    let mut file = File::open(file_path).expect("File exists");
-
-    let mut backup_path = file_arg.clone();
-    backup_path.push_str(".backup");
+    let mut file = File::open(&file_path).expect("File exists");
 
     let mut backup = OpenOptions::new()
                     .write(true)
                     .create(true)
-                    .open(backup_path).expect("Able to write file");
+                    .open(file_path + ".backup").expect("Able to write file");
 
    io::copy(&mut file, &mut backup).expect("Able to copy file to backup");
 }
